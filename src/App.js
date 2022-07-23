44d56686094895e44eb5cac903e7efe96bea5e9b
vsheva <valera.sheva@gmail.com>
2022-07-24 00:05:49 +0300

    Add toggle with redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import {useSelector} from "react-redux";
+import React from "react";
+
 
 function App() {
+   const showCart= useSelector((state)=>state.ui.cartIsVisible)
+
   return (
     <Layout>
-      <Cart />
+        {showCart && <Cart/>}
       <Products />
     </Layout>
   );
--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
 import classes from './CartButton.module.css';
+import {useDispatch} from "react-redux";
+import {uiActions} from "../../store/ui-slice";
 
 const CartButton = (props) => {
+    const dispatch = useDispatch();
+
+    const toggleHandler=()=>{
+       dispatch(uiActions.toggle()) //задиспатчить action, который заведет метод в редюсере
+    }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
