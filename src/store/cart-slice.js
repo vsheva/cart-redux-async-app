9fb6dc1c7bca0a9bbd0401d506ca608ef10f9950
vsheva <valera.sheva@gmail.com>
2022-07-27 00:06:52 +0300

    Add action creator thunk

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {uiActions} from "./ui-slice";
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -38,6 +39,93 @@ const cartSlice = createSlice({
     }
 })
 
+export const sendCartData=(cart)=>{
+    return async (dispatch) => {
+        dispatch(uiActions.showNotification({
+            status: "pending",
+            title: "Sending...",
+            message: "Sending cart data!"
+        }));
+
+        const sendRequest = async () => {
+            const response = await fetch("https://react-http-53159-default-rtdb.firebaseio.com/cart.json", {
+                method: "PUT",
+                body: JSON.stringify(cart),
+                headers: {'Content-Type': 'application/json'}
+            });
+            //const responseData = await response.json();
+            if (!response.ok) {
+                throw new Error("Sending cart data failed!")
+            }
+        };
+
+        try {
+            await sendRequest();
+
+            dispatch(uiActions.showNotification({
+                status: "success",
+                title: "Success!",
+                message: "Sent cart data successfully!"
+            }))
+        } catch (err) {
+            dispatch(uiActions.showNotification({
+                status: "error",
+                title: "Error!",
+                message: "Sending cart data failed!"
+            }))
+        }
+
+    }
+}
+
 export const cartActions =cartSlice.actions
 
 export default cartSlice;
+
+
+//OLD-VERSION
+/*
+import {createSlice} from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {items: [], totalQuantity: 0},
+    reducers: {
+        addItemToCart(state, action) {
+            const existingItem = state.items.find((item) => item.id === action.payload.id) //!*!если нет - добавить, если есть - изменить
+            state.totalQuantity ++;
+            if (!existingItem) {
+                state.items.push({
+                    id: action.payload.id,
+                    title: action.payload.title,
+                    price: action.payload.price,
+                    quantity: 1,
+                    totalPrice: action.payload.price,
+
+                })
+            } else {
+                existingItem.quantity = existingItem.quantity + 1;   //!*! логика изменения количества и цены
+                existingItem.totalPrice = existingItem.totalPrice + action.payload.price;
+            }
+        },
+        removeItemFromCart(state, action) {
+            const id = action.payload
+
+            const existingItem = state.items.find(item => item.id === id)
+            state.totalQuantity --;
+
+            if (existingItem.quantity === 1) {  //удаляем
+                state.items = state.items.filter(item => item.id !== id)
+
+            } else { //вычитаем количество и price
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        }
+    }
+})
+
+export const cartActions =cartSlice.actions
+
+export default cartSlice;
+*/
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,9 +2,14 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const uiSlice=createSlice({
     name: "ui",
-    initialState:{ cartIsVisible: false},
+    initialState:{ cartIsVisible: false, notification:null},
     reducers:{
-        toggle(state){state.cartIsVisible= !state.cartIsVisible}
+        toggle(state){state.cartIsVisible= !state.cartIsVisible},
+        showNotification(state, action) {state.notification={
+            status: action.payload.status,
+            title: action.payload.title,
+            message: action.payload.message
+        }}
     }
 
 });
@@ -12,3 +17,8 @@ const uiSlice=createSlice({
 export const uiActions =uiSlice.actions
 
 export default uiSlice;
+
+
+
+
+//OLD-VERSION
